7f03dd8b9f91326ede6ef21136c20a0d3ddd2bb3
Zbigniew Kostrzewa <zkostrzewa@gmail.com>
2019-09-02 22:22:18 +0200

    netns

--- tests/simple.rs
+++ tests/simple.rs
@@ -1,12 +1,41 @@
+//use futures::Future;
+use getaddrs;
+use libc;
 use membership;
+use tun_tap::Iface;
 
 #[test]
 fn simple_test() {
-    use netsim::spawn;
-    use tokio_core::reactor::Core;
+    //    use netsim::spawn;
 
-    let spawn = spawn::new_namespace(|| {});
-    let mut core = Core::new().unwrap();
-    let interfaces = core.run(spawn_complete).unwrap();
-    assert!(interfaces.is_empty());
+    println!("{}", "OUTSIDE THREAD");
+    for addr in getaddrs::InterfaceAddrs::query_system().unwrap() {
+        println!("{:?}", addr.name);
+    }
+
+    std::thread::spawn(|| {
+        println!("{}", "IN THREAD");
+        unsafe {
+            println!("{}", libc::unshare(libc::CLONE_NEWNET));
+        }
+        let iface = tun_tap::Iface::new("myiface", tun_tap::Mode::Tap).unwrap();
+        for addr in getaddrs::InterfaceAddrs::query_system().unwrap() {
+            println!("{:?}", addr.name);
+        }
+        std::thread::spawn(|| {
+            println!("{}", "IN INNER THREAD");
+            for addr in getaddrs::InterfaceAddrs::query_system().unwrap() {
+                println!("{:?}", addr.name);
+            }
+        })
+        .join();
+    })
+    .join();
+    //    let spawn = spawn::new_namespace(|| {
+    //        println!("{}", "IN NEW NAMESPACE");
+    //        std::thread::spawn(|| {
+    //        })
+    //        .join();
+    //    });
+    //    spawn.wait();
 }
