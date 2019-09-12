75a890f477869d444b337ea7fc3ffbd9109c82cf
Zbigniew Kostrzewa <zkostrzewa@gmail.com>
2019-09-12 06:50:35 +0200

    Add the main example as test.

--- tests/basic.rs
+++ tests/basic.rs
@@ -1,5 +1,8 @@
-use crate::common::get_member_addresses;
+use failure::Error;
 use membership;
+use membership::{Membership, ProtocolConfig};
+use std::net::SocketAddr;
+use std::str::FromStr;
 use std::time::Duration;
 
 mod common;
@@ -14,7 +17,7 @@ fn all_members_alive() {
         std::thread::sleep(Duration::from_secs(10));
 
         for ms in &mss {
-            assert_eq!(addresses, get_member_addresses(ms));
+            assert_eq!(addresses, common::get_member_addresses(ms));
         }
         mss.iter_mut().for_each(|m| m.stop().unwrap());
     });
@@ -30,7 +33,7 @@ fn dead_node_discovered() {
         std::thread::sleep(Duration::from_secs(10));
 
         for ms in &mss {
-            assert_eq!(addresses, get_member_addresses(ms));
+            assert_eq!(addresses, common::get_member_addresses(ms));
         }
 
         mss.pop().unwrap().stop();
@@ -39,9 +42,23 @@ fn dead_node_discovered() {
 
         let expected_addresses = addresses.iter().rev().skip(1).rev().cloned().collect::<Vec<_>>();
         for ms in &mss {
-            assert_eq!(expected_addresses, get_member_addresses(ms));
+            assert_eq!(expected_addresses, common::get_member_addresses(ms));
         }
 
         mss.iter_mut().for_each(|m| m.stop().unwrap());
     });
+}
+
+// FIXME: Add proper checks.
+#[test]
+fn different_ports() -> Result<(), Error> {
+    let mut ms1 = Membership::new(SocketAddr::from_str("127.0.0.1:2345")?, Default::default());
+    let mut ms2 = Membership::new(SocketAddr::from_str("127.0.0.1:3456")?, Default::default());
+    ms1.join(SocketAddr::from_str("127.0.0.1:3456")?)?;
+    ms2.join(SocketAddr::from_str("127.0.0.1:2345")?)?;
+    std::thread::sleep(Duration::from_secs(ProtocolConfig::default().protocol_period * 2));
+    println!("{:?}", ms1.get_members()?);
+    println!("{:?}", ms2.get_members()?);
+    ms1.stop()?;
+    ms2.stop()
 }
