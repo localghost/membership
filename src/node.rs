b39b301825dd2a5cf8d0932a3816323c7de28c50
Zbigniew Kostrzewa <zkostrzewa@gmail.com>
2019-10-02 06:23:48 +0200

    Re-structure project layout (#21)

---
+++ src/node.rs
@@ -0,0 +1,93 @@
+#![deny(missing_docs)]
+
+use crate::result::Result;
+use crate::sync_node::{ChannelMessage, SyncNode};
+use crate::ProtocolConfig;
+use failure::format_err;
+use mio_extras::channel::Sender;
+use std::net::SocketAddr;
+
+/// Runs the gossip protocol on an internal thread.
+pub struct Node {
+    bind_address: SocketAddr,
+    config: Option<ProtocolConfig>,
+    sender: Option<Sender<ChannelMessage>>,
+    handle: Option<std::thread::JoinHandle<Result<()>>>,
+}
+
+impl Node {
+    /// Creates new instance communicating with other members through `bind_address`.
+    pub fn new(bind_address: SocketAddr, config: ProtocolConfig) -> Self {
+        Node {
+            bind_address,
+            config: Some(config),
+            sender: None,
+            handle: None,
+        }
+    }
+
+    /// Returns bind address of this member.
+    pub fn bind_address(&self) -> SocketAddr {
+        self.bind_address
+    }
+
+    /// Joins the group through `member` which has to already belong to the group.
+    ///
+    /// Member might not be instantly spotted by all other members of the group.
+    pub fn join(&mut self, member: SocketAddr) -> Result<()> {
+        assert_ne!(member, self.bind_address, "Can't join yourself");
+        assert!(self.handle.is_none(), "You have already joined");
+
+        let (mut sync_node, sender) = SyncNode::new(self.bind_address, self.config.take().unwrap());
+        self.sender = Some(sender);
+        self.handle = Some(
+            std::thread::Builder::new()
+                .name("membership".to_string())
+                .spawn(move || sync_node.join(member))?,
+        );
+        Ok(())
+    }
+
+    /// Stops this member, removing it from the group.
+    ///
+    /// Member does not broadcast that its quiting (at least not yet), thus it may still be observed by others
+    /// as alive, at least for a short period of time.
+    pub fn stop(&mut self) -> Result<()> {
+        assert!(self.handle.is_some(), "You have not joined yet");
+
+        self.sender
+            .as_ref()
+            .unwrap()
+            .send(ChannelMessage::Stop)
+            .map_err(|e| format_err!("Failed to stop message: {:?}", e))?;
+        self.wait()
+    }
+
+    /// Returns all alive members of the group this member knows about.
+    ///
+    /// These might not necessary be all alive members in the entire group.
+    pub fn get_members(&self) -> Result<Vec<SocketAddr>> {
+        assert!(self.handle.is_some(), "First you have to join");
+
+        let (sender, receiver) = std::sync::mpsc::channel();
+        self.sender
+            .as_ref()
+            .unwrap()
+            .send(ChannelMessage::GetMembers(sender))
+            .map_err(|e| format_err!("Failed to ask for members: {:?}", e))?;
+        receiver
+            .recv()
+            .map_err(|e| format_err!("Failed to get members: {:?}", e))
+    }
+
+    #[doc(hidden)]
+    /// Waits for the member to finish.
+    pub fn wait(&mut self) -> Result<()> {
+        assert!(self.handle.is_some(), "You have not joined yet");
+        self.handle
+            .take()
+            .unwrap()
+            .join()
+            .map_err(|e| format_err!("Membership thread panicked: {:?}", e))?
+    }
+}
--- tests/basic.rs
+++ tests/basic.rs
@@ -1,6 +1,6 @@
 use failure;
 use membership;
-use membership::Membership;
+use membership::Node;
 use std::net::SocketAddr;
 use std::str::FromStr;
 
@@ -30,7 +30,7 @@ fn all_members_alive() -> TestResult {
 
 #[test]
 fn dead_node_discovered() -> TestResult {
-    //    env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
+    env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
     in_namespace(|| -> TestResult {
         let mut members = create_members(3);
         join_neighbours(&mut members)?;
@@ -62,8 +62,8 @@ fn different_ports() -> TestResult {
     in_namespace(|| -> TestResult {
         let address1 = SocketAddr::from_str("127.0.0.1:2345")?;
         let address2 = SocketAddr::from_str("127.0.0.1:3456")?;
-        let mut ms1 = Membership::new(address1, Default::default());
-        let mut ms2 = Membership::new(address2, Default::default());
+        let mut ms1 = Node::new(address1, Default::default());
+        let mut ms2 = Node::new(address2, Default::default());
 
         ms1.join(address2)?;
         ms2.join(address1)?;
