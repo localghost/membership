68b5f3f6bab043bfed719f54c032fb749cef7b64
Zbigniew Kostrzewa <zkostrzewa@gmail.com>
2019-10-16 06:35:20 +0200

    wip encoder

--- src/message_encoder.rs
+++ src/message_encoder.rs
@@ -2,70 +2,92 @@ use crate::member::Member;
 use crate::message::MessageType;
 use crate::notification::Notification;
 use crate::result::Result;
-use bytes::{BufMut, BytesMut};
+use bytes::{BufMut, Bytes, BytesMut, IntoBuf};
 use failure::format_err;
 
-struct RawMessage {
-    buffer: BytesMut,
-}
-
-struct OutgoingMessage {
-    buffer: BytesMut,
-}
+struct OutgoingMessage {}
 
 impl OutgoingMessage {
-    fn new(max_size: usize) -> Self {
-        OutgoingMessage {
+    fn new(max_size: usize) -> OutgoingMessageWithType {
+        OutgoingMessageWithType {
             buffer: BytesMut::with_capacity(max_size),
         }
     }
+}
 
-    fn message_type(&mut self, message_type: MessageType) -> Result<()> {
+struct OutgoingMessageWithType {
+    buffer: BytesMut,
+}
+
+impl OutgoingMessageWithType {
+    fn message_type(mut self, message_type: MessageType) -> Result<OutgoingMessageWithSequenceNumber> {
         if self.buffer.remaining_mut() < std::mem::size_of::<i32>() {
             return Err(format_err!("Could not encode message type"));
         }
         self.buffer.put_i32_be(message_type as i32);
-        Ok(())
+        Ok(OutgoingMessageWithSequenceNumber { buffer: self.buffer })
     }
+}
 
-    fn sequence_number(&mut self, sequence_number: u64) -> Result<()> {
+struct OutgoingMessageWithSequenceNumber {
+    buffer: BytesMut,
+}
+
+impl OutgoingMessageWithSequenceNumber {
+    fn sequence_number(mut self, sequence_number: u64) -> Result<OutgoingMessageWithBuild> {
         if self.buffer.remaining_mut() < std::mem::size_of::<i32>() {
             return Err(format_err!("Could not encode sequence number"));
         }
         self.buffer.put_u64_be(sequence_number);
-        Ok(())
+        Ok(OutgoingMessageWithBuild { buffer: self.buffer })
     }
+}
 
+struct OutgoingMessageWithBuild {
+    buffer: BytesMut,
+}
+
+impl OutgoingMessageWithBuild {
     fn notifications(&mut self, notifications: &[Notification]) -> Result<usize> {
         let mut cursor = std::io::Cursor::new(&self.buffer);
         let count_position = cursor.position();
         for notification in notifications {
-            self.buffer.
-            let position = cursor.position();
-            //            self.encode_notification();
-            self.buffer.put_slice()
+            if self.notification_size(notification) > self.buffer.remaining_mut() {
+                break;
+            }
+            self.encode_notification(notification)?;
+            //            self.buffer.
+            //            let position = cursor.position();
+            //            //            self.encode_notification();
+            //            self.buffer.put_slice()
         }
+        Ok(0)
     }
 
     fn broadcast(members: &[Member]) -> usize {}
 
-    fn build(self) -> RawMessage {
-        RawMessage { buffer: self.buffer }
+    fn build(self) -> Bytes {
+        self.buffer.freeze()
     }
 
-    fn encode_message_type(&mut self, message_type: MessageType) {}
-}
+    fn notification_size(&self, notification: &Notification) -> usize {
+        match notification {
+            Notification::Alive { member } | Notification::Confirm { member } | Notification::Suspect { member } => {
+                if member.address.is_ipv4() {
+                    // notification header + IP address + UDP port + incarnation number
+                    1 + 4 + 2 + 8
+                } else {
+                    unimplemented!()
+                }
+            }
+        }
+    }
 
-//struct MessageEncoder {
-//    max_size: usize,
-//}
-//
-//impl MessageEncoder {
-//    fn new(max_size: usize) -> Self {
-//        MessageEncoder { max_size }
-//    }
-//
-//    fn message(message_type: MessageType, sequence_number: u64) -> OutgoingMessage {
-//        let buffer = BytesMut::with_capacity(max_size);
-//    }
-//}
+    fn encode_notification(&mut self, notification: &Notification) -> Result<()> {
+        Ok(())
+    }
+
+    fn encode_member(&mut self) -> Result<()> {
+        Ok(())
+    }
+}
