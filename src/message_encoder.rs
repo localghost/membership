3ddf53ed1b4a2a9c840e4a4e784bada42410fb13
Zbigniew Kostrzewa <zkostrzewa@gmail.com>
2020-01-16 06:30:22 +0100

    Remove comments.

--- src/message_encoder.rs
+++ src/message_encoder.rs
@@ -16,6 +16,81 @@ macro_rules! size_of_vals {
     };
 }
 
+pub(crate) struct PingRequestMessageEncoder {
+    buffer: BytesMut,
+}
+
+impl PingRequestMessageEncoder {
+    pub(crate) fn new() -> SenderEncoder<SequenceNumberEncoder<TargetEncoder<Self>>> {
+        // TODO: calculate exact max length as capacity
+        let mut buffer = BytesMut::with_capacity(1024);
+        buffer.put_i32_be(MessageType::PingIndirect as i32);
+        SenderEncoder::<SequenceNumberEncoder<TargetEncoder<Self>>>::from(buffer)
+    }
+
+    pub(crate) fn encode(self) -> OutgoingMessage {
+        OutgoingMessage::PingRequestMessage(PingRequestMessageOut {
+            buffer: self.buffer.freeze(),
+        })
+    }
+}
+
+impl From<BytesMut> for PingRequestMessageEncoder {
+    fn from(buffer: BytesMut) -> Self {
+        Self { buffer }
+    }
+}
+
+pub(crate) struct DisseminationMessageEncoder {}
+
+impl DisseminationMessageEncoder {
+    pub(crate) fn new(
+        max_size: usize,
+    ) -> MessageTypeEncoder<SenderEncoder<SequenceNumberEncoder<NotificationsEncoder>>> {
+        MessageTypeEncoder::<SenderEncoder<SequenceNumberEncoder<NotificationsEncoder>>> {
+            buffer: BytesMut::with_capacity(max_size),
+            phantom: PhantomData,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub(crate) struct PingRequestMessageOut {
+    buffer: Bytes,
+}
+
+#[derive(Debug)]
+pub(crate) struct DisseminationMessageOut {
+    buffer: BytesMut,
+    num_notifications: usize,
+    num_broadcast: usize,
+}
+
+impl DisseminationMessageOut {
+    pub(crate) fn num_notifications(&self) -> usize {
+        self.num_notifications
+    }
+
+    pub(crate) fn num_broadcast(&self) -> usize {
+        self.num_broadcast
+    }
+}
+
+#[derive(Debug)]
+pub(crate) enum OutgoingMessage {
+    DisseminationMessage(DisseminationMessageOut),
+    PingRequestMessage(PingRequestMessageOut),
+}
+
+impl OutgoingMessage {
+    pub(crate) fn buffer(&self) -> &[u8] {
+        match self {
+            OutgoingMessage::DisseminationMessage(ref message) => &message.buffer,
+            OutgoingMessage::PingRequestMessage(ref message) => &message.buffer,
+        }
+    }
+}
+
 pub(crate) struct SenderEncoder<T> {
     buffer: BytesMut,
     phantom: std::marker::PhantomData<T>,
@@ -118,171 +193,6 @@ impl<T> From<BytesMut> for SequenceNumberEncoder<T> {
     }
 }
 
-#[derive(Debug)]
-pub(crate) struct PingRequestMessageOut {
-    buffer: Bytes,
-}
-
-#[derive(Debug)]
-pub(crate) struct DisseminationMessageOut {
-    buffer: BytesMut,
-    num_notifications: usize,
-    num_broadcast: usize,
-}
-
-impl DisseminationMessageOut {
-    pub(crate) fn num_notifications(&self) -> usize {
-        self.num_notifications
-    }
-
-    pub(crate) fn num_broadcast(&self) -> usize {
-        self.num_broadcast
-    }
-}
-
-#[derive(Debug)]
-pub(crate) enum OutgoingMessage {
-    DisseminationMessage(DisseminationMessageOut),
-    PingRequestMessage(PingRequestMessageOut),
-}
-
-impl OutgoingMessage {
-    pub(crate) fn buffer(&self) -> &[u8] {
-        match self {
-            OutgoingMessage::DisseminationMessage(ref message) => &message.buffer,
-            OutgoingMessage::PingRequestMessage(ref message) => &message.buffer,
-        }
-    }
-}
-
-fn encode_member(member: &Member, buffer: &mut BytesMut) -> Result<()> {
-    if buffer.remaining_mut() < (std::mem::size_of::<u8>() + size_of_vals!(member.id, member.incarnation)) {
-        return Err(format_err!("Could not encode member"));
-    }
-
-    let position = buffer.len();
-    buffer.put_u8(0u8);
-    buffer.put_slice(member.id.as_slice());
-    buffer.put_u64_be(member.incarnation);
-    match member.address {
-        SocketAddr::V4(address) => {
-            if buffer.remaining_mut() < size_of_vals!(address.ip().octets(), address.port()) {
-                return Err(format_err!("Could not encode member address"));
-            }
-            buffer.put_slice(&address.ip().octets());
-            buffer.put_u16_be(address.port());
-        }
-        SocketAddr::V6(_) => {
-            buffer[position] = 1u8 << 7;
-            todo!();
-        }
-    };
-    Ok(())
-}
-
-//pub(crate) fn encode_message_ping_request(
-//    sender: &Member,
-//    sequence_number: u64,
-//    target: &Member,
-//) -> PingRequestMessageOut {
-//    let mut buffer = BytesMut::new();
-//    // TODO: Header should contain IP type for `sender` and `target`, and number of bytes for Member Id.
-//    let mut header = 0u8;
-//    if sender.address.is_ipv6() {
-//        header |= 1u8 << 7;
-//    }
-//    if target.address.is_ipv6() {
-//        header |= 1u8 << 6;
-//    }
-//    buffer.put_u8(header);
-//    encode_member(sender, &mut buffer);
-//    buffer.put_u64_be(sequence_number);
-//    encode_member(target, &mut buffer);
-//
-//    PingRequestMessageOut {
-//        buffer: buffer.freeze(),
-//    }
-//}
-
-pub(crate) struct PingRequestMessageEncoder {
-    buffer: BytesMut,
-}
-
-impl PingRequestMessageEncoder {
-    pub(crate) fn new() -> SenderEncoder<SequenceNumberEncoder<TargetEncoder<Self>>> {
-        // TODO: calculate exact max length as capacity
-        let mut buffer = BytesMut::with_capacity(1024);
-        buffer.put_i32_be(MessageType::PingIndirect as i32);
-        SenderEncoder::<SequenceNumberEncoder<TargetEncoder<Self>>>::from(buffer)
-    }
-
-    pub(crate) fn encode(self) -> OutgoingMessage {
-        OutgoingMessage::PingRequestMessage(PingRequestMessageOut {
-            buffer: self.buffer.freeze(),
-        })
-    }
-}
-
-impl From<BytesMut> for PingRequestMessageEncoder {
-    fn from(buffer: BytesMut) -> Self {
-        Self { buffer }
-    }
-}
-
-//pub(crate) struct MessageTypeEncoder {
-//    buffer: BytesMut,
-//}
-//
-//impl MessageTypeEncoder {
-//    pub(crate) fn message_type(mut self, message_type: MessageType) -> Result<SenderEncoder> {
-//        if self.buffer.remaining_mut() < std::mem::size_of::<i32>() {
-//            return Err(format_err!("Could not encode message type"));
-//        }
-//        self.buffer.put_i32_be(message_type as i32);
-//        Ok(SenderEncoder { buffer: self.buffer })
-//        //        Ok(SenderEncoder2 {
-//        //            buffer: self.encoder.message.buffer,
-//        //            phantom: PhantomData,
-//        //        })
-//    }
-//}
-//
-//pub(crate) struct SenderEncoder {
-//    buffer: BytesMut,
-//}
-//
-//impl SenderEncoder {
-//    pub(crate) fn sender(mut self, member: &Member) -> Result<SequenceNumberEncoder> {
-//        encode_member(member, &mut self.buffer)?;
-//        Ok(SequenceNumberEncoder { buffer: self.buffer })
-//    }
-//}
-//
-//pub(crate) struct SequenceNumberEncoder {
-//    buffer: BytesMut,
-//}
-//
-//impl SequenceNumberEncoder {
-//    pub(crate) fn sequence_number(mut self, sequence_number: u64) -> Result<NotificationsEncoder> {
-//        if self.buffer.remaining_mut() < size_of_vals!(sequence_number) {
-//            return Err(format_err!("Could not encode sequence number"));
-//        }
-//        self.buffer.put_u64_be(sequence_number);
-//        Ok(NotificationsEncoder {
-//            buffer: self.buffer,
-//            num_notifications: 0,
-//        })
-//    }
-//}
-
-//impl From<BytesMut> for SequenceNumberEncoder {
-//    fn from(_: BytesMut) -> Self {
-//        Self {
-//            encoder: DisseminationMessageEncoder::new(1024).encoder,
-//        }
-//    }
-//}
-
 pub(crate) struct NotificationsEncoder {
     buffer: BytesMut,
     num_notifications: usize,
@@ -383,18 +293,27 @@ impl BroadcastEncoder {
     }
 }
 
-pub(crate) struct DisseminationMessageEncoder {}
-
-impl DisseminationMessageEncoder {
-    pub(crate) fn new(
-        max_size: usize,
-    ) -> MessageTypeEncoder<SenderEncoder<SequenceNumberEncoder<NotificationsEncoder>>> {
-        //        MessageTypeEncoder {
-        //            buffer: BytesMut::with_capacity(max_size),
-        //        }
-        MessageTypeEncoder::<SenderEncoder<SequenceNumberEncoder<NotificationsEncoder>>> {
-            buffer: BytesMut::with_capacity(max_size),
-            phantom: PhantomData,
-        }
+fn encode_member(member: &Member, buffer: &mut BytesMut) -> Result<()> {
+    if buffer.remaining_mut() < (std::mem::size_of::<u8>() + size_of_vals!(member.id, member.incarnation)) {
+        return Err(format_err!("Could not encode member"));
     }
+
+    let position = buffer.len();
+    buffer.put_u8(0u8);
+    buffer.put_slice(member.id.as_slice());
+    buffer.put_u64_be(member.incarnation);
+    match member.address {
+        SocketAddr::V4(address) => {
+            if buffer.remaining_mut() < size_of_vals!(address.ip().octets(), address.port()) {
+                return Err(format_err!("Could not encode member address"));
+            }
+            buffer.put_slice(&address.ip().octets());
+            buffer.put_u16_be(address.port());
+        }
+        SocketAddr::V6(_) => {
+            buffer[position] = 1u8 << 7;
+            todo!();
+        }
+    };
+    Ok(())
 }
